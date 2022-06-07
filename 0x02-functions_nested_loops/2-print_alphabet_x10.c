07ccac5b8dae2fd2fb4e47ab0004ac3f962d7e3c
Stephy <atienostephanie040@gmail.com>
2022-06-07 10:27:32 -0700

    Header file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+*/
+void print_alphabet_x10(void)
+{
+	char a = 'a';
+	int x = 0;
+
+	while (x < 10)
+	{
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
