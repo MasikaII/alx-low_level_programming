4f64c603004219cd5a6fdca495b30c1c8fff952a
Stephy <atienostephanie040@gmail.com>
2022-06-09 04:59:31 -0700

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,10 +8,10 @@ void more_numbers(void)
 	int x, y;
 
 	x = 0;
-	while (x <= 9)
+	while (x < 10)
 	{
 		y = 0;
-		while (y <= 14)
+		while (y < 15)
 		{
 			if (y >= 10)
 				_putchar('0' + j / 10);
