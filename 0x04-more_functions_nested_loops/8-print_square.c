2db5133793ea8c964323ec898c186ee4bde132e7
Stephy <atienostephanie040@gmail.com>
2022-06-09 10:35:04 -0700

    Spongebob square pants

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - prints squares
+ * @size: size of square
+ * Return: void
+ */
+void print_square(int size)
+{
+	int x. y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= size; x++)
+		{
+			_putchar('#');
+			for (y = 1; y < size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
