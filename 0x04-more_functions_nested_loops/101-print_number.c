27a9c7380046ca42984d3685819668b34ebfd3e3
Stephy <atienostephanie040@gmail.com>
2022-06-09 13:15:03 -0700

    Integers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,10 +11,10 @@ void print_number(int n)
 	x = n;
 	if (n < 0)
 	{
-		_putchar(45);
-		x = -n;
+		_putchar('-');
+		x = -x;
 	}
-	if (x / 10)
+	if ((x / 10) > 0)
 		print_number(x / 10);
 	_putchar((x % 10) + '0');
 }
