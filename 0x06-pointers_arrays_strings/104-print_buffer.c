103609b7836f89340ba87bccbd7be1ef2f7dfc47
Stephy <atienostephanie040@gmail.com>
2022-06-16 12:06:19 -0700

    Printing a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,45 +1,78 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * ascii - determines if n is an ASCII character
+ * @n: input
+ * Return: 1 if ASCII 0 otherwise
+ */
+int ascii(int n)
+{
+	return (n >= 32 && n <= 126);
+}
+/**
+ * hexadecimal - prints hexadecimal value of a string
+ * @string: string to be printed
+ * @a: start position
+ * @z: end position
+ * Return: void
+ */
+void hexadecimal(char *string, int a, int z)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		if (x < z)
+			printf("%02x", *(string + a + x));
+		else
+			printf("  ");
+		if (x % 2)
+			printf(" ");
+		x++;
+	}
+}
+/**
+ * ascii_print - prints ascii values for string string
+ * @string: input string
+ * @a: start position
+ * @z: end position
+ * Return: void
+ */
+void ascii_print(char *string, int a, int z)
+{
+	int x, y;
+
+	y = 0;
+	while (y < z)
+	{
+		x = *(string + y + a);
+		if (!ascii(x))
+			x = 46;
+		printf("%c", x);
+		y++;
+	}
+}
+/**
  * print_buffer - prints a buffer
- * @b: buffer being printed
- * @size: number of bytes being printed from the buffer
+ * @b: input string
+ * @size: size of buffer
  * Return: void
  */
 void print_buffer(char *b, int size)
 {
-	int x, y;
+	int a, z;
 
-	x = 0;
-	if (size < 0)
+	if (size > 0)
 	{
+		for (a = 0; a < size; a += 10)
+		{
+			z = (size - a < 10) ? size - a : 10;
+			printf("%08x: ", a);
+			hexadecimal(b, a, z);
+			ascii_print(b, a, z);
+			printf("\n");
+		}
+	}
+	else
 		printf("\n");
-		return;
-	}
-	while (x < size)
-	{
-		if (x % 10 == 0)
-			printf("%08x: ", x);
-		for (y = x; y < x + 9; y += 2)
-		{
-			if ((y < size) && ((y + 1) < size))
-				printf("%02x%02x: ", b[y], b[y + 1]);
-			else
-			{
-				while (++y <= x + 10)
-					printf(" ");
-				printf(" ");
-			}
-		}
-		for (y = x; y < x + 9 && y < size; y++)
-		{
-			if (b[y] >= 32 && b[y] <= 126)
-				printf("%c", b[y]);
-			else
-				printf(".");
-		}
-		printf('\n');
-		x += 10;
-	}
 }
-
