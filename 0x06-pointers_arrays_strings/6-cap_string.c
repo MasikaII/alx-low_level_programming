964f0c82a3e1c131227c99597852f80fd7f632fe
Stephy <atienostephanie040@gmail.com>
2022-06-17 11:39:11 -0700

    Capitalizing words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,37 +8,37 @@
  */
 char *cap_string(char *n)
 {
-        int i;
+	int i;
 
-        i = 0;
-        if (n[0] >= 'a' && n[0] <= 'z')
-        {
-                n[0] += ('A' - 'a');
-        }
-        i = 0;
-        while (n[i])
-        {
-                switch (n[i])
-                {
-                        case ',':
-                        case ';':
-                        case '.':
-                        case '!':
-                        case '?':
-                        case '"':
-                        case '(':
-                        case ')':
-                        case '{':
-                        case '}':
-                        case ' ':
-                        case '\n':
-                        case '\t':
-                                if (n[i + 1] >= 'a' && n[i + 1] <= 'z')
-                                {
-                                        n[i + 1] += ('A' - 'a');
-                                }
-                }
-        i++;
-        }
-        return (n);
+	i = 0;
+	if (n[0] >= 'a' && n[0] <= 'z')
+	{
+		n[0] += ('A' - 'a');
+	}
+	i = 0;
+	while (n[i])
+	{
+		switch (n[i])
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				if (n[i + 1] >= 'a' && n[i + 1] <= 'z')
+				{
+					n[i + 1] += ('A' - 'a');
+				}
+		}
+		i++;
+	}
+	return (n);
 }
